97e834ad448979eca20f603b815ae3b80b8e8d73
Tobias P <tobiasp@live.com>
2025-03-19 11:00:24 +0200

    Initial Commit

---
+++ Solana-Beach/Solana_BeachApp.swift
@@ -0,0 +1,17 @@
+//
+//  Solana_BeachApp.swift
+//  Solana-Beach
+//
+//  Created by Tobias Pechatschek on 19.03.25.
+//
+
+import SwiftUI
+
+@main
+struct Solana_BeachApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Solana-BeachTests/Solana_BeachTests.swift
@@ -0,0 +1,17 @@
+//
+//  Solana_BeachTests.swift
+//  Solana-BeachTests
+//
+//  Created by Tobias Pechatschek on 19.03.25.
+//
+
+import Testing
+@testable import Solana_Beach
+
+struct Solana_BeachTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
